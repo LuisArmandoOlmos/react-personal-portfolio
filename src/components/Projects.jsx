5b9c63b4cb504ae38506cc9209cc8bc0ad5e6e50
LuisArmandoOlmos <luisarmandoolmosolmos@gmail.com>
2022-11-12 13:12:02 -0400

    Add Skills and Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,82 @@
+import todoMachine from "../assets/images/todoMachine.png";
+
 export const Projects = ({ id }) => {
-  const as = id
+  const as = id;
+  const projectsList = [
+    {
+      name: "Todo Machine",
+      projectsImg: todoMachine,
+      projectUrl: "https://luisarmandoolmos.github.io/react-todo-machine/",
+      projectRepository: "https://github.com/LuisArmandoOlmos/react-todo-machine",
+      textP: "It is a web page that helps you manage and check pending tasks.",
+      textAlt: "Todo Machine Image",
+    },
+    {
+      name: "Coming soon",
+      projectsImg: todoMachine,
+      projectUrl: "#",
+      projectRepository: "#",
+      textP: "A new project will be added soon.",
+      textAlt: "Coming soon",
+    },
+    {
+      name: "Coming soon",
+      projectsImg: todoMachine,
+      projectUrl: "#",
+      projectRepository: "#",
+      textP: "A new project will be added soon.",
+      textAlt: "Coming soon",
+    },
+  ];
   return (
-    <div className="min-h-[calc(100vh-50px)] bg-tertiary" id={id}>
-      <h2>Projects page</h2>
+    <div
+      className="bg-tertiary flex flex-col items-center justify-center h-[calc(100vh-50px)] text-white"
+      id={id}
+    >
+      <h2 className="w-[76%] text-xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold mb-1 md:mb-2 lg:mb-3 xl:mb-4">
+        Projects
+      </h2>
+      <div className="bg-secondary border-2 border-primary grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 h-[69vh] overflow-y-auto md:overflow-y-hidden rounded-2xl w-[76%] items-center p-6 md:p-4 gap-3">
+        {projectsList.map(
+          ({
+            name,
+            projectsImg,
+            projectUrl,
+            projectRepository,
+            textP,
+            textAlt,
+          }) => (
+            <div
+              className="bg-tertiary h-[calc(34.5vh-27px)] rounded-lg flex flex-col justify-center text-justify gap-1 p-4 min-h-[200px]"
+              key={name}
+            >
+              <h3 className="text-lg font-medium text-primary">{name}</h3>
+              <p>{textP}</p>
+              <a
+                href={projectUrl}
+                className="text-primary"
+                target="_blank"
+              >Project link </a>
+              <a
+                href={projectRepository}
+                className="text-primary"
+                target="_blank"
+              >Project repository</a>
+            </div>
+          )
+        )}
+        <div
+          className="bg-tertiary h-[calc(34.5vh-27px)] rounded-lg flex flex-col justify-center text-justify gap-1 p-4 min-h-[200px]"
+        >
+          <img
+            alt="Coming soon"
+            className="w-[25%] object-cover object-center hidden"
+            src={todoMachine}
+          />
+          <h3 className="text-lg font-medium text-primary">Coming soon</h3>
+          <p>A new project will be added soon.</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,80 @@
+import logoReact from "../assets/images/logoReact.png";
+import logoHTML5 from "../assets/images/logoHTML5.png";
+import logoCSS3 from "../assets/images/logoCSS3.png";
+import logoJavaScript from "../assets/images/logoJavaScript.png";
+import logoTailwindCSS from "../assets/images/logoTailwindCSS.png";
+import logoGit from "../assets/images/logoGit.png";
+import logoGitHub from "../assets/images/logoGitHub.png";
+import logoIllustrator from "../assets/images/logoIllustrator.png";
+import logoPhotoshop from "../assets/images/logoPhotoshop.png";
+
 export const Skills = ({ id }) => {
+  const skillsList = [
+    {
+      name: "HTML",
+      text: "Logo HTML",
+      logoImg: logoHTML5,
+    },
+    {
+      name: "CSS",
+      text: "Logo CSS",
+      logoImg: logoCSS3,
+    },
+    {
+      name: "JavaScript",
+      text: "Logo JavaScript",
+      logoImg: logoJavaScript,
+    },
+    {
+      name: "React",
+      text: "Logo React",
+      logoImg: logoReact,
+    },
+    {
+      name: "Tailwind",
+      text: "Logo Tailwind",
+      logoImg: logoTailwindCSS,
+    },
+    {
+      name: "Git",
+      text: "Logo Git",
+      logoImg: logoGit,
+    },
+    {
+      name: "GitHub",
+      text: "Logo GitHub",
+      logoImg: logoGitHub,
+    },
+    {
+      name: "Illustrator",
+      text: "Logo Illustrator",
+      logoImg: logoIllustrator,
+    },
+    {
+      name: "Photoshop",
+      text: "Logo Photoshop",
+      logoImg: logoPhotoshop,
+    },
+  ];
   return (
-    <div className="min-h-[calc(100vh-50px)] bg-secondary" id={id}>
-      <h2>Skills page</h2>
+    <div
+      className="bg-secondary flex flex-col items-center justify-center h-[calc(100vh-50px)] text-white"
+      id={id}
+    >
+      <h2 className="w-[76%] text-xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold mb-1 md:mb-2 lg:mb-3 xl:mb-4">
+        Skills
+      </h2>
+      <div className="bg-tertiary border-2 border-primary grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 h-[69vh] overflow-y-auto lg:overflow-y-hidden rounded-2xl w-[76%] p-6 lg:p-4 gap-3 lg:gap-3 items-center">
+        {skillsList.map(({ name, logoImg, text}) => (
+          <div
+            className="bg-secondary flex flex-col items-center justify-center rounded-lg auto-col-max h-[calc(23vh-22px)] p-2"
+            key={name}
+          >
+            <img alt={text} className="h-[50%] mb-1" src={logoImg} />
+            <h3 className="md:text-lg md:font-medium">{name}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
